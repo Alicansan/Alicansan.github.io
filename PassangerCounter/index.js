37342a1bbc574b5cd3d8c58671ae4f300f603ad0
Alican Şan <alicansan2606@gmail.com>
2023-11-26 22:58:54 +0300

    Copied the Passanger Counter
    also made some text changes on profile webpage

---
+++ PassangerCounter/index.js
@@ -0,0 +1,69 @@
+//document.getElementById("count-el").innerText
+
+
+
+let countEl = document.getElementById("count-el")
+let saveEl = document.getElementById("save-el")
+let count = 0  
+//let countHistory = []
+
+function increment () {
+    count = count +1
+    countEl.textContent = count
+}
+
+// function save(){
+//     countHistory.push(count)
+//     console.log(count)
+//     saveEl.innerText= "Previous saves: " + countHistory.join('- ')
+//     count =0
+//     countEl.innerText = count
+// }
+
+// function save(){
+//     let countStr = count + " - "
+//     saveEl.textContent += countStr
+//      let currentDatetime = new Date().toLocaleString()
+//      let saveData = currentDatetime + ": " + count + "\n";
+//      saveEl.textContent += saveData;
+    
+//      count = 0
+//      countEl.textContent = 0
+//  }
+
+function save(){
+
+    let currentDatetime = new Date().toLocaleString()
+
+     saveData = currentDatetime + ": " + count;
+      // Create a new <p> element
+      let saveEntry = document.createElement("p");
+      saveEntry.innerHTML = saveData;
+  
+      // Append the new <p> element to the save element
+      saveEl.appendChild(saveEntry);
+    count = 0
+    countEl.textContent = 0
+}
+function deleteEntries() {
+    
+    while (saveEl.firstChild) 
+    {
+        saveEl.removeChild(saveEl.firstChild)
+    }
+}
+
+
+document.addEventListener('keydown', function (event) {
+    switch (event.key) {
+        case 'a':
+            increment()
+            break
+        case 's':
+            save()
+            break
+        case'd':
+            deleteEntries()
+            break
+    }
+})
